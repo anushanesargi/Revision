4f39c4b457ce431866f9a383a81f07ed61f8d0a8
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-22 08:04:56 +0100

    test case to return close but nope if argument starts with h and ends with e

--- spec/spec_check_codeword.rb
+++ spec/spec_check_codeword.rb
@@ -4,4 +4,8 @@ describe "looks for correct codeword 'horse'" do
   it "returns 'Correct! Come in.' when codeword is horse " do
     expect(check_codeword("horse")).to eq "Correct! Come in."
   end
+
+  it "returns 'Close, but nope.' if codeword starts with h and ends with e" do
+    expect(check_codeword("hope")).to eq "Close, but nope."
+  end
 end
