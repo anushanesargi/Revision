021433f7757bbfedcb608a660f8f97cdbb3e7617
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-22 08:09:26 +0100

    test case to return WRONG! if arg is neither horse nor starts with h and ends with e

--- spec/spec_check_codeword.rb
+++ spec/spec_check_codeword.rb
@@ -8,4 +8,8 @@ describe "looks for correct codeword 'horse'" do
   it "returns 'Close, but nope.' if codeword starts with h and ends with e" do
     expect(check_codeword("hope")).to eq "Close, but nope."
   end
+
+  it "returns 'Wrong' if codeword does not start with h and end with e" do
+    expect(check_codeword("nope")).to eq "WRONG!"
+  end
 end
