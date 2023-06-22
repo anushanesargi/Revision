2ac0c7df1cf16e7c02f282863fc17d73c86a72a3
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-22 16:20:24 +0100

    second test to return the number of words

--- Count_Number_of_Words_in_String/spec/spec_count_words.rb
+++ Count_Number_of_Words_in_String/spec/spec_count_words.rb
@@ -4,4 +4,8 @@ describe "takes a string as an argument and returns the number of words in that 
   it "returns 4 when passed 'Hello! How are you?'" do
     expect(count_words("Hello! How are you?")).to eq 4
   end
+
+  it "returns 1 when passed 'Hello!'" do
+    expect(count_words("Hello!")).to eq 1
+  end
 end
