5af58c1c2968bc4e97a789ceebdaa6cce5241c56
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-22 15:19:38 +0100

    refactored

--- test_driving_a_method_phase_2/lib/make_snippet.rb
+++ test_driving_a_method_phase_2/lib/make_snippet.rb
@@ -1,4 +1,6 @@
 def make_snippet(word)
-  return word if word.length <= 5
-  return word[0...5] + "..." if word.length > 5
+  # return word if word.length <= 5
+  # return word[0...5] + "..." if word.length > 5
+
+  return word.length <= 5 ? word : (word[0...5] + "...")
 end
