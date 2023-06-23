9305127f979061bf5ee0602674e56edd4dc58ed3
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 19:02:46 +0100

    test to return false if text does not start with capital letter and end with punctuation mark

--- test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
@@ -1,7 +1,7 @@
 def test_capital_letter_and_punctutaion(text)
   punctuation = [".", "?", "!"]
 
-  puts text[-1]
+  # puts text[-1]
   # return /[[:alpha:]]/.match(text[0]) ? true : false
   # if punctuation.include? "#{text[-1]}"
   #   return true
--- test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
@@ -32,4 +32,9 @@ describe "verifies that a text starts with a capital letter and ends with a suit
       for a text, assuming that I can read 200 words a minute")).to eq false
   end
 
+  it "returns false for a sentence starting with capital letter and ending with text" do
+    expect(test_capital_letter_and_punctutaion("i want to see an estimate of reading time 
+      for a text, assuming that I can read 200 words a minute")).to eq false
+  end
+
 end
