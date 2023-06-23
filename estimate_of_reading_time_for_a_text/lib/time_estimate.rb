5c1d4a82745918af99c18340222232deb3ca1cb4
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 10:46:09 +0100

    test for more than a minute time estimate

--- estimate_of_reading_time_for_a_text/lib/time_estimate.rb
+++ estimate_of_reading_time_for_a_text/lib/time_estimate.rb
@@ -1,5 +1,6 @@
 def time_estimate(string)
   count = string.split.size
+  puts count
   seconds = count * 60/200.to_f
   minutes = (seconds % 3600)/60
   return "#{minutes.round} minute(s) and #{seconds} second(s)" 
