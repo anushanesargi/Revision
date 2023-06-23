e0fc197b9d10a702bd06ce9ac5cc6ad10aa18515
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 17:39:19 +0100

    passed test to return true if text starts with capital letter

---
+++ test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
@@ -0,0 +1,3 @@
+def test_capital_letter_and_punctutaion(text)
+  return text[0] == text[0].capitalize() ? true : false
+end
--- test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
@@ -2,7 +2,7 @@ require 'test_capital_letter_and_punctutaion'
 
 describe "verifies that a text starts with a capital letter and ends with a suitable sentence-ending punctuation mark." do
 
-  it "returns true" do
-    expect(test_capital_letter_and_punctutaion("I want to see an estimate of reading time for a text, assuming that I can read 200 words a minute.")).to eq true
+  it "returns true for a sentence starting with capital letter" do
+    expect(test_capital_letter_and_punctutaion("I want to see an estimate of reading time for a text, assuming that I can read 200 words a minute")).to eq true
   end
 end
