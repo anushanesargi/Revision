1b3f4bb70406d0f3bab322e926324329863ba3c7
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 18:12:00 +0100

    rearranged comparison statement

--- test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
@@ -1,4 +1,4 @@
 def test_capital_letter_and_punctutaion(text)
   # return /[[:alpha:]]/.match(text[0]) ? true : false
-  return text[0] == text[0].capitalize() && /[[:alpha:]]/.match(text[0]) ? true : false
+  return /[[:alpha:]]/.match(text[0]) && text[0] == text[0].capitalize() ? true : false
 end
