89cd8023e1ef484fc1b6e13721a60787bfcd0a33
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-24 00:13:18 +0100

    test to return nothing to do if no #todo included

--- todo_checks/spec/spec_todo_checks.rb
+++ todo_checks/spec/spec_todo_checks.rb
@@ -6,4 +6,8 @@ describe "check if a text includes the string #TODO" do
     expect(todo_checks("#TODO Get Milk")).to eq "#TODO Get Milk"
   end
 
+  it "returns nothing to do if no #TODO inlcuded" do
+    expect(todo_checks("Got Milk")).to eq "Nothing to do"
+  end
+
 end
