cdae7c101acfb60f80c731c06c97703313113799
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 17:52:14 +0100

    test to return false when text does not start with an alphabet

--- test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
@@ -12,4 +12,9 @@ describe "verifies that a text starts with a capital letter and ends with a suit
       for a text, assuming that I can read 200 words a minute")).to eq false
   end
 
+  it "returns false for a sentence starting with capital letter" do
+    expect(test_capital_letter_and_punctutaion("£ i want to see an estimate of reading time 
+      for a text, assuming that I can read 200 words a minute")).to eq false
+  end
+
 end
