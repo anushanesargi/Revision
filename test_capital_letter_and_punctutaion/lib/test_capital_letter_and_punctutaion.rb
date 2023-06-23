0401cda4a60bc88b322f18ce141d04b453ecd3a4
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-23 19:00:50 +0100

    test to return false if text does not end with appropriate punctuation mark

--- test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/lib/test_capital_letter_and_punctutaion.rb
@@ -3,8 +3,9 @@ def test_capital_letter_and_punctutaion(text)
 
   puts text[-1]
   # return /[[:alpha:]]/.match(text[0]) ? true : false
-  if punctuation.include? "#{text[-1]}"
-    return true
-  end
-  # return /[[:alpha:]]/.match(text[0]) && text[0] == text[0].capitalize() ? true : false
+  # if punctuation.include? "#{text[-1]}"
+  #   return true
+  # end
+  return /[[:alpha:]]/.match(text[0]) && text[0] == text[0].capitalize() && (punctuation.include? "#{text[-1]}")
+
 end
--- test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
+++ test_capital_letter_and_punctutaion/spec/spec_test_capital_letter_and_punctutaion.rb
@@ -17,9 +17,19 @@ describe "verifies that a text starts with a capital letter and ends with a suit
   #     for a text, assuming that I can read 200 words a minute")).to eq false
   # end
 
-  it "returns true for a sentence ending with ." do
-    expect(test_capital_letter_and_punctutaion("@ i want to see an estimate of reading time 
+  # it "returns true for a sentence ending with ." do
+  #   expect(test_capital_letter_and_punctutaion("@ i want to see an estimate of reading time 
+  #     for a text, assuming that I can read 200 words a minute.")).to eq true
+  # end
+
+  it "returns true for a sentence starting with capital letter and ending with ." do
+    expect(test_capital_letter_and_punctutaion("I want to see an estimate of reading time 
       for a text, assuming that I can read 200 words a minute.")).to eq true
+  end
+
+  it "returns false for a sentence starting with capital letter and ending with text" do
+    expect(test_capital_letter_and_punctutaion("I want to see an estimate of reading time 
+      for a text, assuming that I can read 200 words a minute")).to eq false
   end
 
 end
