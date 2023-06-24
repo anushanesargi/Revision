17e6c4640e6d0bbfe390d4fd401a443e734c10df
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-24 12:31:39 +0100

    test to exclude special characters

--- debugging/debugging_get_most_common_letter/spec/spec_get_most_common_letter.rb
+++ debugging/debugging_get_most_common_letter/spec/spec_get_most_common_letter.rb
@@ -1,8 +1,12 @@
 require 'get_most_common_letter'
 
-describe "" do
+describe "Gets the most common letter in a given text" do
+
+  it "return 'o' for the given text" do
+    expect(get_most_common_letter("the roof, the roof, the roof is on fire!")).to eq "o"
+  end
 
   it "" do
-    expect(get_most_common_letter("the roof, the roof, the roof is on fire!")).to eq "o"
+    expect(get_most_common_letter("the swift, fox, jumped over, lazy dog")).to eq "e"
   end
 end
