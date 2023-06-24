336b88f02939d649aeb28f05bf26a47afb64366b
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-24 12:26:48 +0100

    debugged to return most common letter excluding spaces

--- debugging/debugging_get_most_common_letter/lib/get_most_common_letter.rb
+++ debugging/debugging_get_most_common_letter/lib/get_most_common_letter.rb
@@ -1,7 +1,16 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
   text.chars.each do |char|
-    counter[char] += 1
+    if char == " " 
+      next
+    else
+      counter[char] += 1
+    end
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  # puts "#{counter}"
+  # puts "#{counter.to_a}"
+  # puts "#{counter.to_a.sort_by { |k, v| v }}"
+
+  counter = counter.to_a.sort_by { |k, v| v }[-1][0]
+  # puts "#{counter}"
 end
