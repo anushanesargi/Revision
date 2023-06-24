a90894c963535838f1d50fa4e9e00a26bc5e07e6
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-24 12:40:29 +0100

    passed test to include only letters in the alphabet

--- debugging/debugging_get_most_common_letter/lib/get_most_common_letter.rb
+++ debugging/debugging_get_most_common_letter/lib/get_most_common_letter.rb
@@ -1,15 +1,19 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
   text.chars.each do |char|
-    if char == " " 
-      next
-    else
+    # if char = " " 
+    #   next
+    # else
+    #   counter[char] += 1
+    # end
+
+    if /[[:alpha:]]/.match(char)
       counter[char] += 1
     end
   end
   # puts "#{counter}"
   # puts "#{counter.to_a}"
-  # puts "#{counter.to_a.sort_by { |k, v| v }}"
+  puts "#{counter.to_a.sort_by { |k, v| v }}"
 
   counter = counter.to_a.sort_by { |k, v| v }[-1][0]
   # puts "#{counter}"
--- debugging/debugging_get_most_common_letter/spec/spec_get_most_common_letter.rb
+++ debugging/debugging_get_most_common_letter/spec/spec_get_most_common_letter.rb
@@ -2,11 +2,11 @@ require 'get_most_common_letter'
 
 describe "Gets the most common letter in a given text" do
 
-  it "return 'o' for the given text" do
+  it "returns 'o' for the given text" do
     expect(get_most_common_letter("the roof, the roof, the roof is on fire!")).to eq "o"
   end
 
-  it "" do
-    expect(get_most_common_letter("the swift, fox, jumped over, lazy dog")).to eq "e"
+  it "returns 'o' for the given text" do
+    expect(get_most_common_letter("the swift, fox, jumped over, lazy dog")).to eq "o"
   end
 end
