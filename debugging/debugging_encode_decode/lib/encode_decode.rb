fd8b7cada92db23dcd91ec80799f6201dabac585
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-24 10:55:24 +0100

    added print statements

--- debugging/debugging_encode_decode/lib/encode_decode.rb
+++ debugging/debugging_encode_decode/lib/encode_decode.rb
@@ -1,6 +1,8 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
 
+  # puts "#{cipher}"
+
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
@@ -10,8 +12,10 @@ end
 
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  # puts "#{cipher}"
 
   plaintext_chars = ciphertext.chars.map do |char|
+    # puts cipher.ord
     cipher[char.ord - 65]
   end
   return plaintext_chars.join
