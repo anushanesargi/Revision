b4e800be8f765c875f4179a13967131ba8876fb1
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-25 20:50:09 +0100

    test to return the no. of words in the content

--- TDD_diary_entry_class/spec/spec_diary_entry.rb
+++ TDD_diary_entry_class/spec/spec_diary_entry.rb
@@ -6,4 +6,10 @@ RSpec.describe "DiaryEntry" do
     expect(diary_entry.title).to eq "diary_title"
     expect(diary_entry.contents).to eq "diary_contents"
   end
+
+  it "returns the number of words in the contents as an integer" do
+    diary_entry = DiaryEntry.new("diary_title", "diary contents")
+    expect(diary_entry.count_words).to eq "2"
+  end
+
 end
