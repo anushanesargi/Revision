7b3f5aed94d12c547696e873a88fa7bf2814db74
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-25 20:45:23 +0100

    testing contents getter

--- TDD_diary_entry_class/spec/spec_diary_entry.rb
+++ TDD_diary_entry_class/spec/spec_diary_entry.rb
@@ -4,5 +4,6 @@ RSpec.describe "DiaryEntry" do
   it "constructs" do
     diary_entry = DiaryEntry.new("diary_title", "diary_contents")
     expect(diary_entry.title).to eq "diary_title"
+    expect(diary_entry.contents).to eq "diary_contents"
   end
 end
