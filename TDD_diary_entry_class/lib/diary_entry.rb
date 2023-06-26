646165dc129ba811d569ef06ade53f5ffdbefa79
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-26 22:52:06 +0100

    changed the test case for #reading_time

--- TDD_diary_entry_class/lib/diary_entry.rb
+++ TDD_diary_entry_class/lib/diary_entry.rb
@@ -17,7 +17,7 @@ class DiaryEntry
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
-     # user can read per minute
+    # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
   end
--- TDD_diary_entry_class/spec/spec_diary_entry.rb
+++ TDD_diary_entry_class/spec/spec_diary_entry.rb
@@ -26,9 +26,9 @@ end
 RSpec.describe "#reading_time" do
 
   it "Returns an integer representing an estimate of the reading time in minutes for the contents at the given wpm." do
-    diary_entry = DiaryEntry.new("diary_title", "Returns an integer representing an estimate of the reading time")
-    # number_of_words = diary_entry.count_words
-    expect(diary_entry.reading_time(2)).to eq 5
+    diary_entry = DiaryEntry.new("diary_title", "Returns an integer representing an estimate of the reading time" * 100)
+    # number_of_words = diary_entry.count_words"
+    expect(diary_entry.reading_time(200)).to eq 5
   end
 
 end
