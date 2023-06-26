0579853ba61a2540fa98b828a3e023134058f0d5
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-27 00:29:39 +0100

    added a private method for splitting the content also added if else condition in #reading_chunk

--- TDD_diary_entry_class/lib/diary_entry.rb
+++ TDD_diary_entry_class/lib/diary_entry.rb
@@ -2,6 +2,7 @@ class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title # ...
     @contents = contents
+    @count = 0
   end
 
   def title
@@ -13,7 +14,7 @@ class DiaryEntry
   end
 
   def count_words
-    @contents.split.size # Returns the number of words in the contents as an integer
+    content_split.size # Returns the number of words in the contents as an integer
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the user can read per minute
@@ -34,13 +35,27 @@ class DiaryEntry
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning
+    puts "i am here1"
     
-    array_of_words_for_first_chunk = @contents.split
-    # puts number_of_words_for_first_chunk
 
-    # contents[0..number_of_words_for_first_chunk]
-    return array_of_words_for_first_chunk[0, wpm * minutes].join(" ")
+    if @count == 0
+      puts "i am here2"
+      # array_of_words_for_first_chunk = content_split
+      puts "i am here3"
+      chunk_of_words = content_split[0, wpm * minutes].join(" ")
+    else
+      puts "i am here4"
+      return @count
+    end
 
-
+    @count += 1
+    new_content = @contents[wpm * minutes + 1.. ]
+    puts "new content is #{new_content}"
+    return chunk_of_words
   end
+
+  private
+    def content_split
+      @contents.split
+    end
 end
--- TDD_diary_entry_class/spec/spec_diary_entry.rb
+++ TDD_diary_entry_class/spec/spec_diary_entry.rb
@@ -53,6 +53,6 @@ RSpec.describe "#reading_chunk" do
     diary_entry = DiaryEntry.new("diary_title", ("one " * 5 + "two " * 4 + "three " * 5))
     diary_entry.reading_chunk(1, 3)
     expect(diary_entry.reading_chunk(1, 5)).to eq "one one two two two"
-    end
+  end
 
 end
