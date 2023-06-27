19402705e982c9ecd38287a7cc10e40e09afc843
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-28 00:37:46 +0100

    test to return false if text does not end with punctuation mark

--- TDD_garmmar_stats/spec/spec_grammar_stats.rb
+++ TDD_garmmar_stats/spec/spec_grammar_stats.rb
@@ -6,8 +6,13 @@ RSpec.describe "#check" do
   #   expect(grammar_stats.check("Hello!")).to eq "Hello!"
   # end
 
-  it "returns true or false depending on whether the text begins with a capital letter and ends with a sentence-ending punctuation mark." do
+  it "returns true for the text beginning with a capital letter and ends with a sentence-ending punctuation mark." do
     grammar_stats = GrammarStats.new
     expect(grammar_stats.check("Hello!")).to eq true
+  end
+
+  it "returns false for the text beginning with a capital letter and ends with no punctuation mark." do
+    grammar_stats = GrammarStats.new
+    expect(grammar_stats.check("Hello")).to eq false
   end
 end
