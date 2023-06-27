b4ea6001a9cdf1bb18e99d03ca3b14eedc94e9c5
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-28 00:31:10 +0100

    failing test to return true or false depending on whether the text begins with a capital letter and ends with a sentence-ending punctuation mark.

--- TDD_garmmar_stats/spec/spec_grammar_stats.rb
+++ TDD_garmmar_stats/spec/spec_grammar_stats.rb
@@ -1,8 +1,13 @@
 require 'grammar_stats'
 
 RSpec.describe "#check" do
-  it "returns a string" do
+  # it "returns a string" do
+  #   grammar_stats = GrammarStats.new
+  #   expect(grammar_stats.check("Hello!")).to eq "Hello!"
+  # end
+
+  it "returns true or false depending on whether the text begins with a capital letter and ends with a sentence-ending punctuation mark." do
     grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("Hello!")).to eq "Hello!"
+    expect(grammar_stats.check("Hello!")).to eq true
   end
 end
