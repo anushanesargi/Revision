4cad369df7e2b01ad93fcc58be208c7026ec265b
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-28 01:20:44 +0100

    implementation for #percentage_good to return 50 for a true and false checked texts

--- TDD_garmmar_stats/lib/grammar_stats.rb
+++ TDD_garmmar_stats/lib/grammar_stats.rb
@@ -1,34 +1,43 @@
 class GrammarStats
   def initialize
-    @punctuation = [",", ".", "!"]
+    @checked_result = true
     @percentage = 100
-    @no_of_checks = 0
+    @no_of_calls = 0
+    @no_of_true_checks = 0
   end
 
   def check(text)
-    @no_of_checks += 1
+    punctuation = [",", ".", "!"]
+    @no_of_calls += 1
+
+    puts "number of calls #{@no_of_calls}"
 
     if text == ""
+
       return false
+
     else
       if_letter = /[[:alpha:]]/.match(text[0])
       if_capital = (text[0] == text[0].capitalize())
-      if_appropriate_punctuation = @punctuation.include? "#{text[-1]}"
+      if_appropriate_punctuation = punctuation.include? "#{text[-1]}"
       
-      checked = if_letter && if_capital && if_appropriate_punctuation
-
-      # if checked == true
-      #   @percentage = 
-
-      # end
-      return if_letter && if_capital && if_appropriate_punctuation
+      @checked = if_letter && if_capital && if_appropriate_punctuation
 
     end
-   
+
+    if @checked == true
+      @no_of_true_checks += 1
+      puts "number of true checks #{@no_of_true_checks}"
+    end
+
+    
+    return @checked
     
   end
 
   def percentage_good
+
+    @percentage = @percentage * @no_of_true_checks/@no_of_calls
 
     return @percentage
     # Returns as an integer the percentage of texts checked so far that passed
--- TDD_garmmar_stats/spec/spec_grammar_stats.rb
+++ TDD_garmmar_stats/spec/spec_grammar_stats.rb
@@ -1,43 +1,43 @@
 require 'grammar_stats'
 
-RSpec.describe "#check" do
-  # it "returns a string" do
-  #   grammar_stats = GrammarStats.new
-  #   expect(grammar_stats.check("Hello!")).to eq "Hello!"
-  # end
+# RSpec.describe "#check" do
+#   # it "returns a string" do
+#   #   grammar_stats = GrammarStats.new
+#   #   expect(grammar_stats.check("Hello!")).to eq "Hello!"
+#   # end
 
-  it "returns true for the text beginning with a capital letter and ends with a sentence-ending punctuation mark." do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("Hello!")).to eq true
-  end
+#   it "returns true for the text beginning with a capital letter and ends with a sentence-ending punctuation mark." do
+#     grammar_stats = GrammarStats.new
+#     expect(grammar_stats.check("Hello!")).to eq true
+#   end
 
-  it "returns false for the text beginning with a capital letter and ends with no punctuation mark." do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("Hello")).to eq false
-  end
+#   it "returns false for the text beginning with a capital letter and ends with no punctuation mark." do
+#     grammar_stats = GrammarStats.new
+#     expect(grammar_stats.check("Hello")).to eq false
+#   end
 
-  it "returns false for the text not beginning with a capital letter and ends with a sentence-ending punctuation mark." do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("hello!")).to eq false
-  end
+#   it "returns false for the text not beginning with a capital letter and ends with a sentence-ending punctuation mark." do
+#     grammar_stats = GrammarStats.new
+#     expect(grammar_stats.check("hello!")).to eq false
+#   end
 
-  it "returns false for the text not beginning with a capital letter and not ending with a sentence-ending punctuation mark." do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("hello hello")).to eq false
-  end
+#   it "returns false for the text not beginning with a capital letter and not ending with a sentence-ending punctuation mark." do
+#     grammar_stats = GrammarStats.new
+#     expect(grammar_stats.check("hello hello")).to eq false
+#   end
 
-  it "returns false for empty text string" do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("")).to eq false
-  end
-end
+#   it "returns false for empty text string" do
+#     grammar_stats = GrammarStats.new
+#     expect(grammar_stats.check("")).to eq false
+#   end
+# end
 
 RSpec.describe "#percentage_good" do
-  it "retruns 100 if #check is called once returns true" do
-    grammar_stats = GrammarStats.new
-    grammar_stats.check("Hello!")
-    expect(grammar_stats.percentage_good).to eq 100
-  end
+  # it "retruns 100 if #check is called once returns true" do
+  #   grammar_stats = GrammarStats.new
+  #   grammar_stats.check("Hello!")
+  #   expect(grammar_stats.percentage_good).to eq 100
+  # end
 
   it "retruns 50 for a true and false check" do
     grammar_stats = GrammarStats.new
