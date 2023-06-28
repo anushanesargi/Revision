995f6328a3ff2d4630791649db340edd86fd00ca
Anusha Nesargi <anushanesargi@gmail.com>
2023-06-28 01:08:07 +0100

    implementation to return 100 for a true check

--- TDD_garmmar_stats/lib/grammar_stats.rb
+++ TDD_garmmar_stats/lib/grammar_stats.rb
@@ -1,9 +1,13 @@
 class GrammarStats
   def initialize
     @punctuation = [",", ".", "!"]
+    @percentage = 100
+    @no_of_checks = 0
   end
 
   def check(text)
+    @no_of_checks += 1
+
     if text == ""
       return false
     else
@@ -11,14 +15,22 @@ class GrammarStats
       if_capital = (text[0] == text[0].capitalize())
       if_appropriate_punctuation = @punctuation.include? "#{text[-1]}"
       
+      checked = if_letter && if_capital && if_appropriate_punctuation
+
+      # if checked == true
+      #   @percentage = 
+
+      # end
       return if_letter && if_capital && if_appropriate_punctuation
 
     end
-    # text is a string
+   
     
   end
 
   def percentage_good
+
+    return @percentage
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
   end
