2c999617f98f03d4b8f42ae0985d200ed9cb5ebc
Anusha Nesargi <anushanesargi@gmail.com>
2023-07-03 22:59:50 +0100

    failing test to return an empty string for 'seiou'

--- debugging_vowel_remover/spec/vowel_remover_spec.rb
+++ debugging_vowel_remover/spec/vowel_remover_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe "remove_vowels method" do
     result_no_vowels = remover.remove_vowels
     expect(result_no_vowels).to eq "W wll rmv th vwls frm ths sntnc."
   end
+
+  it "removes vowels from a complex string" do
+    remover = VowelRemover.new("aeiou")
+    result_no_vowels = remover.remove_vowels
+    expect(result_no_vowels).to eq ""
+  end
+
 end
