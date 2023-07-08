22828b329522b706a2a47cd230f8ee2c24c93b7d
Anusha Nesargi <anushanesargi@gmail.com>
2023-07-08 09:28:47 +0100

    diary unit test for #count_words

--- test_drive_classes_diary_and_diary_entry/spec/diary_spec.rb
+++ test_drive_classes_diary_and_diary_entry/spec/diary_spec.rb
@@ -6,5 +6,10 @@ describe Diary do
       diary = Diary.new
       expect(diary.all).to eq []
     end
+
+    it "count_words returns zero" do
+      diary = Diary.new
+      expect(diary.count_words).to eq 0
+    end
   end
 end
