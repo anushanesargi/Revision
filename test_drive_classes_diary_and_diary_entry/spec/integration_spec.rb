f46b8f0d9f01cd21a16615b4ea4221aa57b8ebee
Anusha Nesargi <anushanesargi@gmail.com>
2023-07-08 08:14:33 +0100

    failing test to add and return list of diary_entries

--- test_drive_classes_diary_and_diary_entry/spec/integration_spec.rb
+++ test_drive_classes_diary_and_diary_entry/spec/integration_spec.rb
@@ -1,2 +1,14 @@
 require 'diary'
 require 'diary_entry'
+
+
+describe "integration" do
+  context "after adding some entries" do
+    it "lists out the entries added" do
+      diary = Diary.new
+      diary_entry = DiaryEntry.new("my_title", "contents")
+      diary.add(diary_entry)
+      expect(diary.all).to eq [diary_entry]
+    end
+  end
+end
