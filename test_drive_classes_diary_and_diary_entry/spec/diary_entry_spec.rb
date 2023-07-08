cd5c818025f0803d7948340573c3328d9b14a49c
Anusha Nesargi <anushanesargi@gmail.com>
2023-07-08 08:57:26 +0100

    failing diary entry unit test for #count_words

--- test_drive_classes_diary_and_diary_entry/spec/diary_entry_spec.rb
+++ test_drive_classes_diary_and_diary_entry/spec/diary_entry_spec.rb
@@ -1,9 +1,16 @@
 require 'diary_entry'
 
 describe DiaryEntry do
-  it "comstructs" do
+  it "constructs" do
     diary_entry = DiaryEntry.new("my_title", "my_contents")
     expect(diary_entry.title).to eq "my_title"
     expect(diary_entry.contents).to eq "my_contents"
+  end
+
+  describe "#count_words" do
+    it "returns the number of words in contents" do
+      diary_entry = DiaryEntry.new("my_title", "my_contents")
+      expect(diary_entry.count_words).to eq 1
+    end
   end
 end
