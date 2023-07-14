e5fb30a3ed4b4aa03d779ffad79556e63dd1f932
Anusha Nesargi <anushanesargi@gmail.com>
2023-07-15 00:28:56 +0100

    refactoring tests

--- test_drive_classes_diary_and_diary_entry/spec/diary_spec.rb
+++ test_drive_classes_diary_and_diary_entry/spec/diary_spec.rb
@@ -12,4 +12,18 @@ describe Diary do
       expect(diary.count_words).to eq 0
     end
   end
+
+  describe "#reading_time" do
+    context "Returns an integer representing an estimate of the reading time in minutes" do
+      it "raises error for wpm 0" do
+        diary = Diary.new
+        expect { diary.reading_time(0) }.to raise_error "reading speed needs to be positive"
+      end
+
+      it "raises error for wpm -1" do
+        diary = Diary.new
+        expect { diary.reading_time(-1) }.to raise_error "reading speed needs to be positive"
+      end
+    end
+  end
 end
--- test_drive_classes_diary_and_diary_entry/spec/integration_spec.rb
+++ test_drive_classes_diary_and_diary_entry/spec/integration_spec.rb
@@ -24,22 +24,31 @@ describe "integration" do
 
     describe "reading time" do
       context "Returns an integer representing an estimate of the reading time in minutes" do
-        it "fails for 0 wpm" do
-          diary = Diary.new
-          diary_entry_title = DiaryEntry.new("my_title", "contents")
-          diary_entry_next_title = DiaryEntry.new("my_title_next", "double contents")
-          diary.add(diary_entry_title)
-          diary.add(diary_entry_next_title)
-          expect { diary.reading_time(0) }.to raise_error "reading speed needs to be positive"
-        end
+        # it "fails for 0 wpm" do
+        #   diary = Diary.new
+        #   diary_entry_title = DiaryEntry.new("my_title", "contents")
+        #   diary_entry_next_title = DiaryEntry.new("my_title_next", "double contents")
+        #   diary.add(diary_entry_title)
+        #   diary.add(diary_entry_next_title)
+        #   expect { diary.reading_time(0) }.to raise_error "reading speed needs to be positive"
+        # end
 
-        it "fails for negative wpm" do
+        # it "fails for negative wpm" do
+        #   diary = Diary.new
+        #   diary_entry_title = DiaryEntry.new("my_title", "contents")
+        #   diary_entry_next_title = DiaryEntry.new("my_title_next", "double contents")
+        #   diary.add(diary_entry_title)
+        #   diary.add(diary_entry_next_title)
+        #   expect { diary.reading_time(-1) }.to raise_error "reading speed needs to be positive"
+        # end
+
+        it "Returns 2 for wpm 2 and content size 3" do
           diary = Diary.new
-          diary_entry_title = DiaryEntry.new("my_title", "contents")
-          diary_entry_next_title = DiaryEntry.new("my_title_next", "double contents")
+          diary_entry_title = DiaryEntry.new("my_title", "Today is a good day")
+          diary_entry_next_title = DiaryEntry.new("my_title_next", "is partially cloudy")
           diary.add(diary_entry_title)
           diary.add(diary_entry_next_title)
-          expect { diary.reading_time(-1) }.to raise_error "reading speed needs to be positive"
+          expect(diary.reading_time(2)).to eq 4
         end
 
         it "Returns 2 for wpm 2 and content size 3" do
